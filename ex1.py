18e6e5e4065ae8fa57b532df961d5df57c7a7c89
aksanoble <aksanoble@gmail.com>
2014-06-14 23:36:34 +0530

    ex1

--- ex1.py
+++ ex1.py
@@ -1,1 +1,19 @@
-print "Hello World"
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+print "This is another Line."
+
+#print "Hello World!"
+#print "Hello Again"
+print "I like typing this."
+#print "This is fun."
+#print 'Yay! Printing.'
+#print "I'd much rather you 'not'."
+#print 'I "said" do not touch this.'
+#print "This is another Line."
+
+
