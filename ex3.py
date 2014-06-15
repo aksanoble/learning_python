30354780a656781b987b08785c34aae4ea010b8a
aksanoble <aksanoble@gmail.com>
2014-06-15 12:17:22 +0530

    Floating Point

--- ex3.py
+++ ex3.py
@@ -1,25 +1,25 @@
 print "I will now count my chickens:" #This line witll print the sentence
 
-print "Hens", 25 + 30 / 6 #Counting the number of chickens with an expression
-print "Roosters", 100 - 25 *3 % 4 #Counting the number if roosters with an expression
+print "Hens", 25.0 + 30.0 / 6.0 #Counting the number of chickens with an expression
+print "Roosters", 100.0 - 25.0 *3.0 % 4.0 #Counting the number if roosters with an expression
 
 #Python computes mathematical expression and prints the result
 
 print "Now I will count the eggs:" #Printing another string
 
-print 3 + 2 + 1 -5 + 4 %2 -1 / 4 + 6 #Python in this form only proides results in whole numbers
+print 3.0 + 2.0 + 1.0 -5.0 + 4.0 %2.0 -1.0 / 4.0 + 6.0 #Python in this form only proides results in whole numbers
 
-print "Is it true that 3 + 2 < 5 - 7" #Printing a question for python followed by the question itself.
+print "Is it true that 3.0 + 2.0 < 5.0 - 7.0" #Printing a question for python followed by the question itself.
 
-print 3 + 2 < 5 - 7  #When you present a conditional expression, python evaluates and gives the answer in terms of True or False
+print 3 + 2 < 5.0 - 7.0  #When you present a conditional expression, python evaluates and gives the answer in terms of True or False
 
-print "What is 3 + 2?", 3 + 2 #Printing a question and then evaluating the expression
-print "What is 5 - 7?", 5 - 7 #Printing a question and then evaluating the expression
+print "What is 3.0 + 2?", 3.0 + 2 #Printing a question and then evaluating the expression
+print "What is 5 - 7?", 5.0 - 7 #Printing a question and then evaluating the expression
 
 print "Oh, that's why it's False." #Printing, more printing
 
 print "How about some more." #Setting up for more print and computing simple expression
 
-print "Is it greater?" , 5 > -2 #Print and evaluate expression for True or False
-print "Is it greater or equal?", 5>= -2 #Printing a True/False question and getting the answer by letting python compute it
-print "Is it less or equal?", 5 <= -2 #Printing a True/False question also , makes it print in the same line
+print "Is it greater?" , 5.0 > -2 #Print and evaluate expression for True or False
+print "Is it greater or equal?", 5.0>= -2 #Printing a True/False question and getting the answer by letting python compute it
+print "Is it less or equal?", 5.0 <= -2 #Printing a True/False question also , makes it print in the same line
