49e8dbe4895fa70fb3e5bb5e2dcca13821dbc9b9
aksanoble <aksanoble@gmail.com>
2014-06-15 19:27:05 +0530

    comments

--- ex7.py
+++ ex7.py
@@ -1,23 +1,41 @@
+#Print a string
 print "Mary had a little lamb."
+#Print a string
 print "Its fleece was white as %s." % 'snow'
+#Print a string
 print "And everywhere that Mary went."
+#Print a string
 print "." * 10 #what'd that do?
 
+#Assign letter to a variable
 end1 = "C"
+#Assign letter to a variable
 end2 = "h"
+#Assign letter to a variable
 end3 = "e"
+#Assign letter to a variable
 end4 = "e"
+#Assign letter to a variable
 end5 = "s"
+#Assign letter to a variable
 end6 = "e"
+#Assign letter to a variable
 end7 = "B"
+#Assign letter to a variable
 end8 = "u"
+#Assign letter to a variable
 end9 = "r"
+#Assign letter to a variable
 end10 = "g"
+#Assign letter to a variable
 end11 = "e"
+#Assign letter to a variable
 end12 = "r"
 
 #watch that comma at the end. try removing it to see what happens
-
+#Adding different string, the plus sign removes space in between
 print end1 + end2 + end3 + end4 + end5 + end6,
+#Adding different stings, the plus sign brings letters together while the
+#keeps the two lines print on the same line?
 print end7 + end8 + end9 + end10 + end11 + end12
 
